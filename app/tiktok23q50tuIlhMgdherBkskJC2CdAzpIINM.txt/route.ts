0220dd677e9dab236d20d25222e59f0b61d0b260
Daniel <bdanielparedesf@gmail.com>
2026-09-14 13:44:38 -0300

    Delete app/tiktok23q50tuIlhMgdherBkskJC2CdAzpIINM.txt/route.ts and keep TikTok verification as static file in public/

--- app/tiktok23q50tuIlhMgdherBkskJC2CdAzpIINM.txt/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return new Response('tiktok-developers-site-verification=23q50tuIlhMgdherBkskJC2CdAzpIINM', {
-    headers: { 'Content-Type': 'text/plain' },
-  });
-}
-
