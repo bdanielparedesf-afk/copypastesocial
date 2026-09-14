187daeee5d828d6d6469a3965580bbc0e69dc6d1
Daniel <bdanielparedesf@gmail.com>
2026-09-14 13:24:54 -0300

    fix: tiktok verification files with correct token 23q50tuIlhMgdherBkskJC2CdAzpIINM

---
+++ app/tiktok23q50tuIlhMgdherBkskJC2CdAzpIINM.txt/route.ts
@@ -0,0 +1,6 @@
+export async function GET() {
+  return new Response('tiktok-developers-site-verification=23q50tuIlhMgdherBkskJC2CdAzpIINM', {
+    headers: { 'Content-Type': 'text/plain' },
+  });
+}
+
